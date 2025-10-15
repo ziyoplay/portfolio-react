a59ac1352a128cb1e4b70255a8b87334b26d1489
Ziyodulla <ziyoplay@gmail.com>
2025-10-15 17:51:25 +0500

    bug fixed

--- src/Components/Resume/ResumeBanner/index.jsx
+++ src/Components/Resume/ResumeBanner/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './style.css'
+import { Link } from 'react-router-dom'
 const ResumeBanner = () => {
   return (
     <div className='resumeBanner container'>
         <h1>Graduation Behind. <br /> Dedication Ahead.</h1>
         <p>Designing a mobile app to connect food enthusiasts through shared dining experiences, from concept to prototypFresh out of university with a zeal for UX/UI design, I'm keen to use my skills to craft engaging mobile app interfaces. I seek a role that challenges me and lets me contribute to exciting projects, nurturing my growth in design.e.</p>
-        <a href=""> Download Resume as PDF</a>
+        <Link to="/"> Download Resume as PDF</Link>
     </div>
   )
 }
